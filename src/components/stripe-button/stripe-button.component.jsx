bd849718b3a25bb5c58d1623cf979d7ade3205f3
Jeffrey Lui <jlui820@gmail.com>
2020-06-27 16:40:06 -0700

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51Gy6HIJ2hrBBSiBeh4ABH7gxNbWODki9vfrj40J3XYJTsh22KoKlnca85yNuGnyQ7mv06P2Xu3Zo6UV0dpSmNhhO00W8UHq14E';
+  
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='TuckerBc-Shop'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
